e67514fd6e2e670b331298e9dd56e15d570afd17
Tom <r@gaudion.co>
2018-07-16 23:52:36 +0900

    Fix bug q1-4

--- questions/chapter01/q1-4/answer.cpp
+++ questions/chapter01/q1-4/answer.cpp
@@ -11,30 +11,33 @@ bool Answer(string s1){
 
   int odd_count = 0;
   int space_count = 0;
-  string res = "false";
+  bool res = false;
 
-  for( int i=0; i < s1.size() - 1; i++ ){
-    int same_count = 0;
+  for( int i=0; i < s1.size(); i++ ){
+    bool isOdd = true;
 
-      if( s1[i] == ' ' ){
-        space_count++;
-      } else{
-        for( int j=0; j < s1.size(); j++ ){
-          if( tolower(s1[i]) == tolower(s1[j]) )
-            same_count++;
+    if( s1[i] == ' ' ){
+      space_count++;
+    } else if( s1[i] != '\0' ){
+      for( int j= i + 1; j < s1.size(); j++ ){
+        if( tolower(s1[i]) == tolower(s1[j]) ){
+          isOdd = false;
+          s1[j] = '\0';
+          break;
         }
       }
-    if( (same_count % 2) == 1 ){
-      odd_count++;
+      if( isOdd ){
+        odd_count++;
+      }
     }
   }
 
   if( (s1.size() - space_count) % 2 == 0 ){
     if( odd_count == 0 )
-      res = "true";
+      res = true;
   }else{
     if( odd_count == 1 )
-      res = "true";
+      res = true;
   }
 
   return res;
