a994b4f59563d48a999baeaa50473d1295676eeb
Tom <r@gaudion.co>
2018-07-16 22:11:12 +0900

    Add q1-2 solution

--- questions/chapter01/q1-2/answer.cpp
+++ questions/chapter01/q1-2/answer.cpp
@@ -4,5 +4,24 @@ using namespace std;
 // other.
 
 bool CheckPermutation(string s1, string s2){
+  if( s1.size() != s2.size() ){
+    return false;
+  }
+
+  for( int i=0; i < s1.size(); i++)
+  {
+    for( int j=0; j < s2.size(); j++)
+    {
+      if( s1[i] == s2[j] ){
+        s2[j] = 0;
+      }
+    }
+  }
+
+  for( int i=0; i < s2.size(); i++){
+    if( s2[i] != 0 )
+      return false;
+  }
+
   return true;
 }
