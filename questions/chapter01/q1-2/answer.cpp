d6ece07ca02d188c27a56a79b71350fb97e589dc
Wilson <ws@gaudiolab.com>
2018-07-17 08:49:20 +0900

    Solve q1-2

--- questions/chapter01/q1-2/answer.cpp
+++ questions/chapter01/q1-2/answer.cpp
@@ -4,5 +4,25 @@ using namespace std;
 // other.
 
 bool CheckPermutation(string s1, string s2){
+
+  map<char, unsigned int> c;
+
+  if (s1.size() != s2.size())
+    return false;
+
+  for (size_t i = 0; i < s1.size(); i++) {
+    c[s1[i]]++;
+  }
+
+  for (auto iter = c.begin(); iter != c.end(); iter++) {
+    int count = 0;
+    for (size_t i = 0; i < s2.size(); i++) {
+        if (iter->first == s2[i])
+          count++;
+    }
+    if (count != iter->second)
+      return false;
+  }
+
   return true;
 }
