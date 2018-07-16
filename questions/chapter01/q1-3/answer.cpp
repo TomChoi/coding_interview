b46164b5d1027ef4ab435299210997d251ced0e3
Tom <r@gaudion.co>
2018-07-16 22:25:30 +0900

    Add q1-3 solution

--- questions/chapter01/q1-3/answer.cpp
+++ questions/chapter01/q1-3/answer.cpp
@@ -1,5 +1,14 @@
 using namespace std;
 
 string Answer(string s1, int len){
-  return "";
+
+  string res;
+  for( int i=0; i < 13; i++ ){
+    if( s1[i] == ' ' )
+      res += "%20";
+    else
+      res += s1[i];
+  }
+
+  return res;
 }
