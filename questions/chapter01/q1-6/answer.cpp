32e4765506465e36459663207dd19e40f3cf1f93
Tom <r@gaudion.co>
2018-07-17 09:30:12 +0900

    Add Q6 solution

--- questions/chapter01/q1-6/answer.cpp
+++ questions/chapter01/q1-6/answer.cpp
@@ -6,5 +6,22 @@ using namespace std;
 // the original string. You can assume the string has only uppercase and lowercase letters (a - z).
 
 string Answer(string s1){
-  return "";
+  string res;
+  for(int i=0; i<s1.size();){
+    int same_count = 0;
+    res += s1[i];
+    for(int j=i; j<s1.size(); j++){
+      if( s1[i] == s1[j] ){
+        same_count++;
+        i = j;
+      }else{
+        i = j;
+        break;
+      }
+    }
+    res += to_string(same_count);
+    if( i == s1.size() -1 )
+      break;
+  }
+  return res;
 }
