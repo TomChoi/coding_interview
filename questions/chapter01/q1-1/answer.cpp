e7619c639c0d5fd628ba4a41f39d98458e54e7b8
Tom <r@gaudion.co>
2018-07-17 22:14:34 +0900

    Add Q1 solution

--- questions/chapter01/q1-1/answer.cpp
+++ questions/chapter01/q1-1/answer.cpp
@@ -4,5 +4,13 @@ using namespace std;
 // cannot use additional data structures?
 
 bool Answer(string s1){
+
+  for( int i=0; i<s1.size() - 1; i++ ){
+    for( int j=i + 1; j<s1.size(); j++){
+      if( s1[i] == s1[j] ){
+        return false;
+      }
+    }
+  }
   return true;
 }
