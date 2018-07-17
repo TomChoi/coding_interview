581fa1bdfdc654eff5ac4bddbbbe55013a3ffb4e
Tom <r@gaudion.co>
2018-07-17 10:33:49 +0900

    Add Q8 solution

--- questions/chapter01/q1-8/answer.cpp
+++ questions/chapter01/q1-8/answer.cpp
@@ -6,6 +6,21 @@ using namespace std;
 // column are set to 0.
 
 vector<int> Answer(vector<int> v1, int row, int columm){
-  
-  return v1;
+  vector<int> res(v1);
+  for(int i=0; i < v1.size(); i++){
+    if( v1[i] == 0 ){
+      int target_col = i % columm;
+      int target_row = i / columm;
+      for(int j=0; j<v1.size(); j++){
+        if( j / columm == target_row ){
+          res[j] = 0;
+        }else{
+          if( j % columm == target_col){
+            res[j] = 0;
+          }
+        }
+      }
+    }
+  }
+  return res;
 }
