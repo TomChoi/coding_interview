75ede1e0f54d2e8139f16bfd3588e1e14543a427
Wilson <ws@gaudiolab.com>
2018-07-18 09:06:19 +0900

    Solve q1-4

--- questions/chapter01/q1-4/answer.cpp
+++ questions/chapter01/q1-4/answer.cpp
@@ -8,5 +8,54 @@ using namespace std;
 // Output: True (permutations: "taco cat", "atco eta", etc.)
 
 bool Answer(string s1){
-  return true;
+
+  string cs;
+
+  char sp = ' ';
+  for (size_t i = 0; i < s1.size(); i++) {
+    if (s1[i] != sp) {
+      cs += s1[i];
+    }
+  }
+
+  std::string ls(cs);
+  std::transform(ls.begin(), ls.end(), ls.begin(), ::tolower);
+
+  int expectedOddNumberCount = 0;
+  if (ls.size() % 2  == 0) {
+    expectedOddNumberCount = 0;
+  } else {
+    expectedOddNumberCount = 1;
+  }
+
+  vector<int> toggle(ls.size(), 1);
+
+  for (size_t i = 0; i < ls.size(); i++) {
+    if (toggle[i] == 1) {
+      bool hasFound = false;
+      for (size_t j = i + 1; j < ls.size(); j++) {
+        if (ls[i] == ls[j])
+        {
+          toggle[j] = 0;
+          hasFound = true;
+          break;
+        }
+      }
+      if (hasFound) {
+        toggle[i] = 0;
+      }
+    }
+  }
+
+  int oddNumberCount = 0;
+  for (size_t i = 0; i < toggle.size(); i++) {
+    if (toggle[i] == 1) {
+      oddNumberCount++;
+    }
+  }
+
+  if (expectedOddNumberCount == oddNumberCount)
+    return true;
+  else
+    return false;
 }
