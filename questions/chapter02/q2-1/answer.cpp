8ae8cef8d82211bff7de8eaeec942c422d2f64dd
Tom <r@gaudion.co>
2018-07-18 09:16:56 +0900

    Add Chapter02 test case

---
+++ questions/chapter02/q2-1/answer.cpp
@@ -0,0 +1,11 @@
+#include "LinkedList.h"
+
+using namespace std;
+
+// Is Unique: Implement an algorithm to determine if a string has all unique characters. What if you
+// cannot use additional data structures?
+
+bool Answer(string s1){
+  
+  return true;
+}
---
+++ questions/chapter02/q2-2/answer.cpp
@@ -0,0 +1,10 @@
+#include "LinkedList.h"
+
+using namespace std;
+
+// Check Permutation: Given two strings, write a method to decide if one is a permutation of the
+// other.
+
+bool CheckPermutation(string s1, string s2){
+  return true;
+}
