ad0125882573b0b7cc414619520a462f41e3c4ac
Tom <r@gaudion.co>
2018-07-20 18:56:39 +0900

    Add chapter02 Q4

--- questions/chapter02/q2-3/test_case/test_case.cpp
+++ questions/chapter02/q2-3/test_case/test_case.cpp
@@ -3,5 +3,82 @@
 #include "../answer.cpp"
 
 TEST_CASE( "Coding test Chapter02", "[Question03]" ) {
+  SECTION("1"){
+    LinkedList<char> input;
+    input.appendNode('a');
+    input.appendNode('b');
+    input.appendNode('c');
+    input.appendNode('d');
+    input.appendNode('e');
+    input.appendNode('f');
 
+    LinkedList<char> res;
+    res.appendNode('a');
+    res.appendNode('b');
+    res.appendNode('d');
+    res.appendNode('e');
+    res.appendNode('f');
+
+    REQUIRE( DeleteMiddleNode(input) == res );
+  }
+
+  SECTION("2"){
+    LinkedList<char> input;
+    input.appendNode('a');
+    input.appendNode('A');
+    input.appendNode('a');
+    input.appendNode('a');
+    input.appendNode('A');
+    input.appendNode('a');
+
+    LinkedList<char> res;
+    res.appendNode('a');
+    res.appendNode('A');
+    res.appendNode('a');
+    res.appendNode('A');
+    res.appendNode('a');
+
+    REQUIRE( DeleteMiddleNode(input) == res );
+  }
+
+  SECTION("3"){
+    LinkedList<char> input;
+    input.appendNode('a');
+
+    LinkedList<char> res;
+    res.appendNode('a');
+
+    REQUIRE( DeleteMiddleNode(input) == res );
+  }
+
+  SECTION("4"){
+    LinkedList<char> input;
+    input.appendNode('a');
+    input.appendNode('b');
+
+    LinkedList<char> res;
+    res.appendNode('a');
+    res.appendNode('b');
+
+    REQUIRE( DeleteMiddleNode(input) == res );
+  }
+
+  SECTION("5"){
+    LinkedList<char> input;
+    input.appendNode('1');
+    input.appendNode('A');
+    input.appendNode('a');
+    input.appendNode(1);
+    input.appendNode('A');
+    input.appendNode(' ');
+
+    LinkedList<char> res;
+    res.appendNode('1');
+    res.appendNode('A');
+    res.appendNode(1);
+    res.appendNode('A');
+    res.appendNode(' ');
+
+    REQUIRE( DeleteMiddleNode(input) == res );
+  }
 }
