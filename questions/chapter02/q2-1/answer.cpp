7e4efa00c72367e36758a11e82dac697ad6771bc
Tom <r@gaudion.co>
2018-07-20 17:42:38 +0900

    Add test case of chapter02 Q1

--- questions/chapter02/q2-1/answer.cpp
+++ questions/chapter02/q2-1/answer.cpp
@@ -10,6 +10,11 @@ struct Node{
 public:
   T data;
   Node* next;
+
+  Node(T d){
+    data = d;
+    next = nullptr;
+  }
 };
 
 template< class T >
@@ -17,18 +22,61 @@ class LinkedList
 {
 public:
   Node<T>* head;
-  int length;
 
-  LinkedList();
-  ~LinkedList();
+  LinkedList(){
+    head = nullptr;
+  }
+
+  ~LinkedList(){
+    // release all node
+  }
+
+  bool operator==(const LinkedList& other) const {
+    Node<T>* l_node = head;
+    Node<T>* r_node = other.head;
+
+    if( getLength() != other.getLength() ){
+      return false;
+    }
+
+    for( int i=0; i< getLength(); i++ ){
+      if( l_node->data != r_node->data ){
+        return false;
+      }
+      l_node = l_node->next;
+      r_node = r_node->next;
+    }
+    return true;
+  }
+
+  int getLength() const {
+    int count = 0;
+    if( head != nullptr ){
+      count = 1;
+      Node<T>* currentNode = head;
+      while( currentNode->next != nullptr){
+        currentNode = currentNode->next;
+        count++;
+      }
+    }
+    return count;
+  }
 
   void appendNode(T data){
-
+    if( head == nullptr){
+      head = new Node<T>(data);
+    }else{
+      Node<T>* newNode = new Node<T>(data);
+      Node<T>* currentNode = head;
+      while( currentNode->next != nullptr ){
+        currentNode = currentNode->next;
+      };
+      currentNode->next = newNode;
+    }
   }
-private:
+
+  void Answer(){
+    // wirte your answer
+    
+  }
 };
-
-bool Answer(Node<char>& Head){
-
-  return true;
-}
--- questions/chapter02/q2-1/test_case/test_case.cpp
+++ questions/chapter02/q2-1/test_case/test_case.cpp
@@ -3,8 +3,104 @@
 #include "../answer.cpp"
 
 TEST_CASE( "Coding test Chapter02", "[Question01]" ) {
-  SECTION(""){
-    Node<char> node;
-    REQUIRE( Answer(node) == true );
+  SECTION("int"){
+    LinkedList<int> list;
+    list.appendNode(9);
+    list.appendNode(3);
+    list.appendNode(4);
+    list.appendNode(3);
+    list.appendNode(1);
+
+    LinkedList<int> res;
+    res.appendNode(9);
+    res.appendNode(3);
+    res.appendNode(4);
+    res.appendNode(1);
+
+    list.Answer();
+
+    REQUIRE( list == res );
   }
+
+  SECTION("float"){
+    LinkedList<float> list;
+    list.appendNode(9.33);
+    list.appendNode(3.123);
+    list.appendNode(4.11);
+    list.appendNode(4.21);
+    list.appendNode(3.123);
+    list.appendNode(2.99);
+
+    LinkedList<float> res;
+    res.appendNode(9.33);
+    res.appendNode(3.123);
+    res.appendNode(4.11);
+    res.appendNode(4.21);
+    res.appendNode(2.99);
+
+    list.Answer();
+
+    REQUIRE( list == res );
+  }
+
+  SECTION("char"){
+    LinkedList<float> list;
+    list.appendNode('c');
+    list.appendNode('v');
+    list.appendNode('a');
+    list.appendNode('v');
+    list.appendNode('a');
+    list.appendNode('a');
+    list.appendNode('a');
+    list.appendNode('a');
+    list.appendNode('a');
+    list.appendNode('c');
+    list.appendNode('c');
+
+    LinkedList<float> res;
+    res.appendNode('c');
+    res.appendNode('a');
+    res.appendNode('v');
+
+    list.Answer();
+
+    REQUIRE( list == res );
+  }
+
+  SECTION("mix"){
+    LinkedList<char> list;
+    list.appendNode('a');
+    list.appendNode('a');
+    list.appendNode(4);
+    list.appendNode('a');
+    list.appendNode(32);
+    list.appendNode(23);
+    list.appendNode('a');
+    list.appendNode(4);
+    list.appendNode(4);
+    list.appendNode('1');
+    list.appendNode(4);
+    list.appendNode(23);
+    list.appendNode(' ');
+    list.appendNode('a');
+    list.appendNode(4);
+    list.appendNode('1');
+    list.appendNode(23);
+    list.appendNode('1');
+    list.appendNode('1');
+    list.appendNode('1');
+
+    LinkedList<char> res;
+    res.appendNode('a');
+    res.appendNode(32);
+    res.appendNode('1');
+    res.appendNode(23);
+    res.appendNode(4);
+    res.appendNode(' ');
+
+    list.Answer();
+
+    REQUIRE( list == res );
+  }
+
 }
