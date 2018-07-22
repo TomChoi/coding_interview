fae2dd141aa916bcac32ddc1f22f240337d53c28
Tom <r@gaudion.co>
2018-07-22 11:00:04 +0900

    Add answer chapter02 Q1

--- questions/chapter02/q2-1/answer.cpp
+++ questions/chapter02/q2-1/answer.cpp
@@ -74,9 +74,35 @@ public:
       currentNode->next = newNode;
     }
   }
+
+  void deleteNode(Node<T>* node){
+    delete node;
+  }
 };
 
 LinkedList<char>& RemoveDups(LinkedList<char>& list){
   // wirte your answer
+  Node<char>* current = list.head;
+  Node<char>* prev = nullptr;
+  Node<char>* willDeletedNode = nullptr;
+
+  int TOTAL_ASCII_NUMBER = 128;
+  vector<bool> isDupliate(TOTAL_ASCII_NUMBER, false);
+
+  while( current != nullptr ){
+    if( isDupliate[current->data] ){
+      prev->next = current->next;
+      willDeletedNode = current;
+    }else{
+      isDupliate[current->data] = true;
+      prev = current;
+    }
+
+    current = current->next;
+    if( willDeletedNode != nullptr ){
+      list.deleteNode(willDeletedNode);
+      willDeletedNode = nullptr;
+    }
+  }
   return list;
 }
