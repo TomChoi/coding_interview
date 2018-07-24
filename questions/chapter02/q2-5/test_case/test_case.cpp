22a4d2790c8e7681ad3b4c29354297691450451e
Tom <r@gaudion.co>
2018-07-25 08:15:21 +0900

    fix q2-5 test case

--- questions/chapter02/q2-5/test_case/test_case.cpp
+++ questions/chapter02/q2-5/test_case/test_case.cpp
@@ -68,18 +68,18 @@ TEST_CASE( "Coding test Chapter02", "[Question05]" ) {
 
 
     LinkedList<int> answer;
-    input2.appendNode(1);
-    input2.appendNode(3);
-    input2.appendNode(3);
-    input2.appendNode(7);
-    input2.appendNode(3);
-    input2.appendNode(3);
-    input2.appendNode(3);
-    input2.appendNode(3);
+    answer.appendNode(1);
+    answer.appendNode(3);
+    answer.appendNode(3);
+    answer.appendNode(7);
+    answer.appendNode(3);
+    answer.appendNode(3);
+    answer.appendNode(3);
+    answer.appendNode(3);
 
     REQUIRE( SumLists(input1, input2) == answer);
   }
-  
+
   SECTION("4"){
     LinkedList<int> input1;
     input1.appendNode(8);
