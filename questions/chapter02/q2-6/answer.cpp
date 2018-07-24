de6b48a37a8829a04d5f9775b544932853f74e99
Tom <r@gaudion.co>
2018-07-24 23:35:32 +0900

    fix q2-6 error

--- questions/chapter02/q2-6/answer.cpp
+++ questions/chapter02/q2-6/answer.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 // Palindrome: Implement a function to check if a linked list is a palindrome.
 
-bool Palindrome(LinkedList<int>& list){
+bool Palindrome(LinkedList<char>& list){
 
   return true;
 }
