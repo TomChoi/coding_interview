cd1da1e56decdc62dbb285910b404d8c83dca865
Wilson <ws@gaudiolab.com>
2018-07-30 10:05:00 +0900

    Solve q2-8

--- questions/chapter02/q2-8/answer.cpp
+++ questions/chapter02/q2-8/answer.cpp
@@ -2,15 +2,38 @@
 
 using namespace std;
 
-// Loop Detection: Given a circular linked list, implement an algorithm that returns the node at the
+// Loop Detection: Given a circular linked list,
+// implement an algorithm that returns the node at the
 // beginning of the loop.
 // DEFINITION
-// Circular linked list: A (corrupt) linked list in which a node's next pointer points to an earlier node, so as to make a loop in the linked list.
+// Circular linked list:
+// A (corrupt) linked list in which a node's next pointer points to an earlier node,
+// so as to make a loop in the linked list.
 // EXAMPLE
 // Input: A -> B -> C -> D -> E -> C[thesameCasearlier]
 // Output: C
 
 Node<char>* LoopDetection(LinkedList<char>& list){
 
-  return list.head;
+  Node<char>* head = list.head;
+  Node<char>* reference_node = head->next;
+  Node<char>* current_node = reference_node->next;
+
+  bool hasFound = false;
+  while (reference_node != nullptr) {
+    while (current_node != nullptr) {
+      if (current_node == reference_node) {
+        hasFound = true;
+        break;
+      }
+      current_node = current_node->next;
+    }
+    if (hasFound) {
+      break;
+    } else {
+      reference_node = reference_node->next;
+      current_node = reference_node->next;
+    }
+  }
+  return reference_node;
 }
